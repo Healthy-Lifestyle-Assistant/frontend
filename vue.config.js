d5a8851ced2aaec63800e7de34bf73d7180f3039
OlegKDev <OlegKDeveloper@gmail.com>
2023-07-10 13:10:55 +0300

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  devServer: {
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api/v1": "/api/v1"
+        }
+      }
+    }
+  }
+});
