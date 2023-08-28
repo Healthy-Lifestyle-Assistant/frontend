e697d8c124098da5bd64652e0d02ea4fa6758318
OlegKDev <OlegKDeveloper@gmail.com>
2023-08-28 12:16:51 +0300

    Get exercises #8

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <h2>Healthy Lifestyle App</h2>
   <p>Your assistant to track workouts and nutrition</p>
-  
-  <router-link to="/about">About</router-link> | 
+
+  <router-link to="/about">About</router-link> |
   <router-link to="/hello">Hello</router-link> |
   <router-link to="/signup">Signup</router-link> |
-  <router-link to="/login">Login</router-link>
+  <router-link to="/login">Login</router-link> |
+  <router-link to="/workouts">Workouts</router-link>
   <router-view></router-view>
 
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
@@ -19,7 +20,7 @@
 // import About from "./views/About.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   // components: {
   //   HelloWorld,
   //   Hello,
---
+++ src/views/WorkoutsMenuView.vue
@@ -0,0 +1,10 @@
+<template>
+    <h2>Workouts Menu</h2>
+    <router-link to="/exercises">Exercises</router-link>
+</template>
+
+<script>
+export default {
+    name: "WorkoutsMenuView"
+}
+</script>
