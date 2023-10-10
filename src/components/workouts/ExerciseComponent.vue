7cab8af62c399323fc5e400e7eac7c0e1cce4073
Oleg Kuchrenko <OlegKDeveloper@gmail.com>
2023-10-10 15:26:44 +0300

    Update Exercises Page Unlogged #25 (#28)

--- src/components/workouts/ExerciseComponent.vue
+++ src/components/workouts/ExerciseComponent.vue
@@ -1,10 +1,20 @@
 <template>
-    <div>
-        <h4>Exercise Component</h4>
-        <p>Title - {{ title }}</p>
-        <p>Description - {{ description }}</p>
-        <p>Body parts - {{ bodyParts }}</p>
-        <p>Type - {{ type }}</p>
+    <div class="card w-25 mb-3 me-2 bg-dark text-info" style="min-width: 16rem;">
+        <div class="card-body">
+            <div class="card-title">{{ title }}</div>
+
+            <p class="card-text">{{ description }}</p>
+
+            <p class="card-text">{{ type }}</p>
+
+            <div v-if="bodyParts" class="card-text mb-2">
+                <span v-for="bodyPart in bodyParts" :key="bodyPart.id">{{ bodyPart.name }}&nbsp;</span>
+            </div>
+
+            <div class="d-flex justify-content-end">
+                <a href="#" class="btn btn-outline-info" target="_blank">Details</a>
+            </div>
+        </div>
     </div>
 </template>
   
@@ -16,7 +26,7 @@ export default {
         id: Number,
         title: String,
         description: String,
-        bodyParts: String,
+        bodyParts: Array,
         type: String
     }
 }
