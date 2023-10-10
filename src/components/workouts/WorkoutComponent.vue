087c2433c682d925bb9521942b442fcfd6f9b126
Oleg Kuchrenko <OlegKDeveloper@gmail.com>
2023-10-10 15:10:06 +0300

    Update Workouts Start Page Unlogged #22 (#27)

---
+++ src/components/workouts/WorkoutComponent.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h4>Workout Component</h4>
+		<p>Title - {{ title }}</p>
+		<p>Description - {{ description }}</p>
+		<p>Body parts - {{ bodyParts }}</p>
+		<p>Type - {{ type }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "WorkoutComponent",
+
+	props: {
+		id: Number,
+		title: String,
+		description: String,
+		bodyParts: String,
+		type: String
+	}
+}
+</script>
+
+<style></style>
---
+++ src/views/workouts/WorkoutsStartPage.vue
@@ -0,0 +1,100 @@
+<template>
+    <metainfo>
+        <template v-slot:title="{ content }">{{ content }}</template>
+    </metainfo>
+
+    <div class="d-flex">
+        <router-link to="" class="btn btn-info me-4" role="button">Create Custom Workout</router-link>
+        <router-link to="/workouts-exercises" class="btn btn-info me-4" role="button">Exercises</router-link>
+        <router-link to="" class="btn btn-info me-4" role="button">Reminders</router-link>
+    </div>
+
+    <br><br>
+    <div v-if="workouts">
+        <div v-for="workout in workouts" :key="workout.id">
+            <WorkoutComponent :title="workout.title" :description="workout.description" :bodyParts="workout.bodyParts" :type="workout.type">
+            </WorkoutComponent>
+        </div>
+    </div>
+</template>
+
+<script>
+import { useMeta } from "vue-meta";
+import WorkoutComponent from "../../components/workouts/WorkoutComponent.vue";
+
+export default {
+    name: "WorkoutsStartPage",
+
+    setup() {
+        useMeta({
+            title: "Workouts Menu",
+            htmlAttrs: {
+                lang: "en"
+            }
+        })
+    },
+
+    data() {
+        return {
+            workouts: null,
+            message: ""
+        };
+    },
+
+    async created() {
+        this.$store.commit("setCurrentUrl", "/workouts-start-page");
+
+        this.workouts = [
+            {
+                "id": 1,
+                "title": "Workout Title 1",
+                "description": "Workout Description 1",
+                "bodyParts": "BodyPart1, BodyPart2",
+                "type": "Default"
+            },
+            {
+                "id": 2,
+                "title": "Workout Title 2",
+                "description": "Workout Description 2",
+                "bodyParts": "BodyPart3, BodyPart4",
+                "type": "Default"
+            },
+            {
+                "id": 3,
+                "title": "Workout Title 3",
+                "description": "Workout Description 3",
+                "bodyParts": "BodyPart5, BodyPart6",
+                "type": "Default"
+            }
+        ];
+
+        // const res = await this.getDefaultWorkouts();
+        // this.workouts = res;
+    },
+
+    components: {
+        WorkoutComponent
+    },
+
+    // methods: {
+    // async getDefaultWorkouts() {
+    // let URL = "/api/v1/workouts/default";
+
+    // const res = await fetch(URL, {
+    //     method: "GET",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     }
+    // });
+
+    // const data = await res.json();
+
+    // return {
+    //     status: res.status,
+    //     body: data
+    // };
+    // }
+    // }
+
+}
+</script>
