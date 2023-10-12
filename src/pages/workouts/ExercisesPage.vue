e3c385b2c7c8ee4cd55af39a31eb05d1d57b749b
Oleg Kuchrenko <OlegKDeveloper@gmail.com>
2023-10-12 18:51:54 +0300

    Update Get Default Exercises #34 (#35)

--- src/pages/workouts/ExercisesPage.vue
+++ src/pages/workouts/ExercisesPage.vue
@@ -16,7 +16,7 @@
 
             <div v-for="exercise in exercises" :key="exercise.id">
                 <ExerciseComponent :title="exercise.title" :description="exercise.description"
-                    :bodyParts="exercise.bodyParts" :type="exercise.type" />
+                    :bodyParts="exercise.bodyParts" :isCustom="exercise.custom" />
             </div>
 
         </div>
@@ -57,126 +57,136 @@ export default {
     async created() {
         this.$store.commit("setCurrentUrl", "/workouts-exercises");
 
-        this.exercises = [
-            {
-                "id": 1,
-                "title": "Exercise Title 1",
-                "description": "Exercise Description 1",
-                "bodyParts": [
-                    {
-                        "id": 1,
-                        "name": "BodyPart1"
-                    },
-                    {
-                        "id": 2,
-                        "name": "BodyPart2"
-                    }
-                ],
-                "type": "Default"
-            },
-            {
-                "id": 2,
-                "title": "Exercise Title 2",
-                "description": "Exercise Description 2",
-                "bodyParts": [
-                    {
-                        "id": 3,
-                        "name": "BodyPart3"
-                    },
-                    {
-                        "id": 4,
-                        "name": "BodyPart4"
-                    }
-                ],
-                "type": "Default"
-            },
-            {
-                "id": 3,
-                "title": "Exercise Title 3",
-                "description": "Exercise Description 3",
-                "bodyParts": [
-                    {
-                        "id": 5,
-                        "name": "BodyPart5"
-                    }
-                ],
-                "type": "Default"
+        const isTokenValid = await this.validateToken();
+
+        if (isTokenValid) {
+            this.$store.commit("setLogged", true);
+            // 
+        } else {
+            try {
+                this.$store.commit("setLogged", false);
+
+                const res = await this.getDefaultExercises();
+
+                if (res.status === 200) {
+                    this.exercises = res.body;
+                }
+                else if (res.status === 401) {
+                    this.$router.push("/login");
+                }
+                else {
+                    this.message = `Unexpected response status (${res.status})`;
+                }
+            } catch (error) {
+                this.message = "An error occurred while signing up. Try again";
             }
-        ];
+        }
 
-        // const res = await this.getDefaultExercisesApi();
-        // this.entities = res.body;
-
-        // const token = localStorage.getItem("token");
-        // if (token === null || token === "") this.$router.push("/login");
-        // else {
-        //     try {
-        //         const res = await this.getExercisesApi(token, false);
-        //         if (res.status === 200) {
-        //             this.entities = res.body.exercises;
-        //         }
-        //         else if (res.status === 401) {
-        //             this.$store.commit('setLogged', false);
-        //             this.$router.push("/login");
-        //         }
-        //         else {
-        //             this.$store.commit('setLogged', false);
-        //             this.message = `Unexpected response status (${res.status})`;
-        //         }
-        //     } catch (error) {
-        //         this.message = "An error occurred while signing up. Try again";
-        //         console.error(error);
+        // this.exercises = [
+        //     {
+        //         "id": 1,
+        //         "title": "Exercise Title 1",
+        //         "description": "Exercise Description 1",
+        //         "bodyParts": [
+        //             {
+        //                 "id": 1,
+        //                 "name": "BodyPart1"
+        //             },
+        //             {
+        //                 "id": 2,
+        //                 "name": "BodyPart2"
+        //             }
+        //         ],
+        //         "type": "Default"
+        //     },
+        //     {
+        //         "id": 2,
+        //         "title": "Exercise Title 2",
+        //         "description": "Exercise Description 2",
+        //         "bodyParts": [
+        //             {
+        //                 "id": 3,
+        //                 "name": "BodyPart3"
+        //             },
+        //             {
+        //                 "id": 4,
+        //                 "name": "BodyPart4"
+        //             }
+        //         ],
+        //         "type": "Default"
+        //     },
+        //     {
+        //         "id": 3,
+        //         "title": "Exercise Title 3",
+        //         "description": "Exercise Description 3",
+        //         "bodyParts": [
+        //             {
+        //                 "id": 5,
+        //                 "name": "BodyPart5"
+        //             }
+        //         ],
+        //         "type": "Default"
         //     }
-        // }
+        // ];
     },
 
     computed: {
         isSuccess() {
-            return this.message.includes('success');
+            return this.message.includes("success");
         },
 
         isError() {
-            return this.message.includes('error');
+            return this.message.includes("error");
         }
     },
 
-    // methods: {
-    //     async getExercisesApi(token, isCustomOnly) {
-    //         let URL = "/api/v1/exercises";
-    //         if (isCustomOnly) URL = URL + "?isCustomOnly=true"
-    //         const res = await fetch(URL, {
-    //             method: "GET",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 "Authorization": `Bearer ${token}`
-    //             }
-    //         });
+    methods: {
+        async validateToken() {
+            const token = localStorage.getItem("token");
+        
+            if (token === null || token === "") {
+                return false; 
+            } else {
+                // GET /api/v1/auth/validate
+                return  false;
+            }
+        },
 
-    //         const data = await res.json();
-    //         return {
-    //             status: res.status,
-    //             body: data
-    //         };
-    //     },
+        async getDefaultExercises() {
+            let URL = "/api/v1/workouts/exercises/default";
 
-    //     async getDefaultExercisesApi() {
-    //         let URL = "/api/v1/exercises/default";
+            const res = await fetch(URL, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
 
-    //         const res = await fetch(URL, {
-    //             method: "GET",
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             }
-    //         });
+            const data = await res.json();
 
-    //         const data = await res.json();
+            return {
+                status: res.status,
+                body: data
+            };
+        }
+        // async getExercisesApi(token, isCustomOnly) {
+        //     let URL = "/api/v1/exercises";
+        //     if (isCustomOnly) URL = URL + "?isCustomOnly=true"
+        //     const res = await fetch(URL, {
+        //         method: "GET",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //             "Authorization": `Bearer ${token}`
+        //         }
+        //     });
 
-    //         return {
-    //             status: res.status,
-    //             body: data
-    //         };
-    //     }
-    // }
+        //     const data = await res.json();
+        //     return {
+        //         status: res.status,
+        //         body: data
+        //     };
+        // }
+    }
 };
 </script>
 
