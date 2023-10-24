21a63e0caefdde850192d2a0b7cfa7016f3a8196
Oleg Kucherenko <OlegKDeveloper@gmail.com>
2023-10-24 15:59:46 +0300

    Update Create Exercise Page #46 (#47)

--- src/pages/common/common.js
+++ src/pages/common/common.js
@@ -3,3 +3,30 @@ export function getToken() {
     if (token === null || token === "") return null;
     return token;
 }
+
+export async function getAndValidateToken() {
+    const token = localStorage.getItem("token");
+
+    if (token === null) return null;
+
+    if (token === "") {
+        localStorage.removeItem("token");
+        return null;
+    }
+
+    let URL = "/api/v1/users/auth/validate";
+
+    const res = await fetch(URL, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    });
+
+    if (res.status === 200) return token;
+
+    localStorage.removeItem("token");
+    
+    return null;
+}
