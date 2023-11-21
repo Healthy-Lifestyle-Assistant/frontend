4bd6502d5f706a8cefbf3c6f390140f9b7e264cc
Anastasiia Shevchuk <shevchuk2834@gmail.com>
2023-11-21 18:32:48 +0200

    Manage User Profile Page #63

--- src/users/pages/SettingsPage.vue
+++ src/users/pages/SettingsPage.vue
@@ -4,39 +4,39 @@
     </metainfo>
 
     <div class="d-flex flex-column align-items-center">
-        <p>Commin soon...</p>
+        
 
         <AlertComponent :message="message" :messageType="messageType" />
 
         <form @submit.prevent="submitForm" style="width: fit-content;" class="mb-5">
             <div class="mb-4">
                 <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" v-model="username" ref="username" required>
+                <input type="text" class="form-control" id="username" v-model="username" ref="username" >
             </div>
 
             <div class="mb-4">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" v-model="email" ref="email" required>
+                <input type="email" class="form-control" id="email" v-model="email" ref="email" >
             </div>
 
             <div class="mb-4">
                 <label for="fullName" class="form-label">Name</label>
-                <input type="text" class="form-control" id="fullName" v-model="fullName" ref="fullName" required>
+                <input type="text" class="form-control" id="fullName" v-model="fullName" ref="fullName" >
             </div>
 
             <div class="mb-4">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" v-model="password" ref="password" placeholder="********" required>
+                <input type="password" class="form-control" id="password" v-model="password" ref="password"  >
             </div>
 
             <div class="mb-4">
-                <label for="confirmPassword" class="form-label">Confirm Password></label>
-                <input type="password" class="form-control" id="confirmPassword" v-model="confirmPassword" ref="confirmPassword" placeholder="********" required>
+                <label for="confirmPassword" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="confirmPassword" v-model="confirmPassword" ref="confirmPassword" >
             </div>
 
             <div v-if="countries" class="mb-4">
                 <label for="countries" class="form-label">Country</label>
-                <select id="countries" v-model="countryId" class="form-select" aria-label="Select Country" required>
+                <select id="countries" v-model="countryId" class="form-select" aria-label="Select Country" ref="countryId" >
                     <option v-for="elt in countries" :key="elt.id" :value="elt.id">{{ elt.name }}</option>
                 </select>
             </div>
@@ -46,22 +46,23 @@
                 <input type="age" class="form-control" id="age" v-model="age" ref="age">
             </div>
 
-            <div>
-                <span style="color: red;">*</span> Required Fields
-            </div>
-
             <button type="submit" class="btn btn-secondary mt-4" @click="submitForm">Update</button>
-             <button type="button" class="btn btn-secondary mt-4"  @click="submitFormDelete">Delete</button>
             <br><br>
         </form> 
+
+        <form v-if="!isDeleted" @submit.prevent="submitFormDelete" style="min-width: 13rem; width: fit-content;"
+            class="mb-5">
+            <button type="submit" class="btn btn-warning mt-4">Delete</button>
+        </form>
+        <br>
     </div>
 </template>
 
 <script>
 import { useMeta } from "vue-meta";
-import { getToken } from "../../share/js/common.js";
-import { getAndValidateToken } from "../../share/js/common.js";
-import AlertComponent from "../../share/components/AlertComponent.vue";
+import { getToken } from "../../shared/js/common.js";
+import { getAndValidateToken } from "../../shared/js/common.js";
+import AlertComponent from "../../shared/components/AlertComponent.vue";
 
 
 export default {
@@ -78,6 +79,7 @@ export default {
 
      data() {
         return {
+            id: null,
             username: "",
             email: "",
             fullName: "",
@@ -87,7 +89,17 @@ export default {
             countries: null,
             age: null,
             message: "",
-            messageType: ""
+            messageType: "",
+            oldUsername: "",
+            oldEmail: "",
+            oldFullName: "",
+            oldPassword: "",
+            oldConfirmPassword: "",
+            oldCountryId: null,
+            oldCountries: null,
+            oldAge: null,
+            isDeleted: false
+
         };
     },
 
@@ -118,13 +130,13 @@ export default {
     methods: {
         async submitForm() {
             const requestDto = {
-                updatedName: this.username,
-                updatedEmail: this.email,
-                updatedfullName: this.fullName,
-                updatedPassword: this.password,
-                confirmPassword: this.password,
-                updatedCountryId: this.countryId,
-                updatedAge: this.age
+                updatedUsername: this.username == this.oldUsername ? null : this.username,
+                updatedEmail: this.email == this.oldEmail ? null : this.email,
+                updatedFullName: this.fullName == this.oldFullName ? null : this.fullName,
+                updatedPassword: this.password == this.oldPassword ? null : this.password,
+                updatedConfirmPassword: this.password == this.oldConfirmPassword ? null : this.password,
+                updatedCountryId: this.countryId == this.oldCountryId ? null : this.countryId,
+                updatedAge: this.age == this.oldAge ? null : this.age
             };
 
             try {
@@ -134,7 +146,7 @@ export default {
                     this.messageType = "SUCCESS";
                     this.message = "Profile has been updated successfully";
 
-                    this.name = res.body.name;
+                    this.username = res.body.username;
                     this.email = res.body.email;
                     this.fullName = res.body.fullName;
                     this.password = res.body.password;
@@ -203,14 +215,21 @@ export default {
             });
 
             const data = await res.json();
-
+            this.id = data.id;
             this.username = data.username;
+            this.oldUsername = data.username;
             this.email = data.email;
+            this.oldEmail = data.email;
             this.fullName = data.fullName;
+            this.oldFullName = data.fullName;
             this.password = data.password;
+            this.oldPassword = data.password;
             this.confirmPassword = data.password;
+            this.oldConfirmPassword = data.password;
             this.countryId = data.countryId;
+            this.oldCountryId = data.countryId;
             this.age = data.age;
+            this.oldAge = data.age;
 
              this.$nextTick(() => {
         this.$refs.username.placeholder = data.username; 
@@ -225,7 +244,7 @@ export default {
         },
 
         async updateUserDetails(requestBody) {
-            let URL = `/api/v1/users/${this.$route.params.id}`;
+            let URL = `/api/v1/users/${this.id}`;
             let token = getToken();
 
             const res = await fetch(URL, {
@@ -238,6 +257,23 @@ export default {
             });
 
             const data = await res.json();
+            if (res.status === 200) {
+                this.id = data.id;
+                this.username = data.username;
+                this.oldUsername = data.username;
+                this.email = data.email;
+                this.oldEmail = data.email;
+                this.fullName = data.fullName;
+                this.oldFullName = data.fullName;
+                this.password = data.password;
+                this.oldPassword = data.password;
+                this.confirmPassword = data.password;
+                this.oldConfirmPassword = data.password;
+                this.countryId = data.countryId;
+                this.oldCountryId = data.countryId;
+                this.age = data.age;
+                this.oldAge = data.age;
+            }
 
             return {
                 status: res.status,
@@ -245,8 +281,8 @@ export default {
             };
         },
 
-        async deleteUser(id) {
-            let URL = `/api/v1/users/${id}`;
+        async deleteUser() {
+            let URL = `/api/v1/users/${this.id}`;
             let token = getToken();
 
             const res = await fetch(URL, {
@@ -256,11 +292,15 @@ export default {
                     "Authorization": `Bearer ${token}`
                 }
             });
-
+            this.$store.commit("setCurrentUrl", "/login");
+            localStorage.removeItem("token");
+            this.$store.commit("setLogged", false);
+            this.$router.push("/login");
             return {
                 status: res.status
             };
         },
+    
     }
 };
 
