633c845b2534e2cc3d5a0a4604ec3640e6895038
OlegKDev <OlegKDeveloper@gmail.com>
2023-12-11 21:06:37 +0200

    Update user

--- src/users/pages/SettingsPage.vue
+++ src/users/pages/SettingsPage.vue
@@ -1,110 +1,65 @@
 <template>
-  <metainfo>
-    <template v-slot:title="{ content }">{{ content }}</template>
-  </metainfo>
+	<metainfo>
+		<template v-slot:title="{ content }">{{ content }}</template>
+	</metainfo>
 
-  <div class="d-flex flex-column align-items-center">
-    <AlertComponent :message="message" :messageType="messageType" />
-    <form
-      v-if="!isDeleted"
-      class="mb-5"
-      @submit.prevent="submitForm"
-      style="width: fit-content"
-    >
-      <div class="mb-4">
-        <label for="username" class="form-label">Username</label>
-        <input
-          type="text"
-          class="form-control"
-          id="username"
-          v-model="username"
-          ref="username"
-        />
-      </div>
+	<div class="d-flex flex-column align-items-start">
+		<AlertComponent :message="message" :messageType="messageType" />
 
-      <div class="mb-4">
-        <label for="email" class="form-label">Email</label>
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          v-model="email"
-          ref="email"
-        />
-      </div>
+		<h4 class="mb-4">Update Profile</h4>
 
-      <div class="mb-4">
-        <label for="fullName" class="form-label">Name</label>
-        <input
-          type="text"
-          class="form-control"
-          id="fullName"
-          v-model="fullName"
-          ref="fullName"
-        />
-      </div>
+		<form class="mb-5" @submit.prevent="submitForm" style="width: fit-content">
+			<div class="mb-4">
+				<label for="username" class="form-label"><b>Username:</b> {{ this.usernameLabel }}</label>
+				<input type="text" class="form-control" id="username" v-model="username" placeholder="New Username" />
+			</div>
 
-      <div class="mb-4">
-        <label for="password" class="form-label">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          v-model="password"
-          ref="password"
-        />
-      </div>
+			<div class="mb-4">
+				<label for="email" class="form-label"><b>Email:</b> {{ this.emailLabel }}</label>
+				<input type="email" class="form-control" id="email" v-model="email" placeholder="New Email" />
+			</div>
 
-      <div class="mb-4">
-        <label for="confirmPassword" class="form-label">Confirm Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="confirmPassword"
-          v-model="confirmPassword"
-          ref="confirmPassword"
-        />
-      </div>
+			<div class="mb-4">
+				<label for="fullName" class="form-label"><b>Full Name:</b> {{ this.fullNameLabel }}</label>
+				<input type="text" class="form-control" id="fullName" v-model="fullName" placeholder="New Full Name" />
+			</div>
 
-      <div v-if="countries" class="mb-4">
-        <label for="countries" class="form-label">Country</label>
-        <select
-          id="countries"
-          v-model="countryId"
-          class="form-select"
-          aria-label="Select Country"
-          ref="countryId"
-        >
-          <option v-for="elt in countries" :key="elt.id" :value="elt.id">
-            {{ elt.name }}
-          </option>
-        </select>
-      </div>
+			<div class="mb-4">
+				<label for="password" class="form-label"><b>Update Password</b></label>
+				<input type="password" class="form-control" id="password" v-model="password" placeholder="New Password" />
+			</div>
 
-      <div class="mb-4">
-        <label for="age" class="form-label">Age</label>
-        <input
-          type="age"
-          class="form-control"
-          id="age"
-          v-model="age"
-          ref="age"
-        />
-      </div>
+			<div class="mb-4">
+				<label for="confirmPassword" class="form-label"><b>Confirm Update Password</b></label>
+				<input type="password" class="form-control" id="confirmPassword" v-model="confirmPassword"
+					placeholder="New Password" />
+			</div>
 
-      <button type="submit" class="btn btn-secondary mt-4">Update</button>
-    </form>
+			<div class="mb-4">
+				<label for="age" class="form-label"><b>Age:</b> {{ this.ageLabel === null ? "Optional" : this.ageLabel
+				}}</label>
+				<input type="age" class="form-control" id="age" v-model="age" placeholder="New Age" />
+			</div>
 
-    <form
-      v-if="!isDeleted"
-      @submit.prevent="submitFormDelete"
-      style="min-width: 13rem; width: fit-content"
-      class="mb-5"
-    >
-      <button type="submit" class="btn btn-warning mt-4">Delete</button>
-    </form>
-    <br />
-  </div>
+			<div v-if="countries" class="mb-4">
+				<label for="countries" class="form-label"><b>Country</b></label>
+				<select id="countries" v-model="countryId" class="form-select" aria-label="Select Country" required>
+					<option v-for="elt in countries" :key="elt.id" :value="elt.id">
+						{{ elt.name }}
+					</option>
+				</select>
+			</div>
+
+			<button type="submit" class="btn btn-secondary mt-4">Update</button>
+		</form>
+
+		<h4 class="mb-2">Delete Profile</h4>
+
+		<form @submit.prevent="submitFormDelete" style="min-width: 13rem; width: fit-content" class="mb-5">
+			<button type="submit" class="btn btn-warning mt-4">Delete</button>
+		</form>
+		<br />
+	</div>
 </template>
 
 <script>
@@ -114,242 +69,214 @@ import { getAndValidateToken } from "../../shared/js/common.js";
 import AlertComponent from "../../shared/components/AlertComponent.vue";
 
 export default {
-  name: "SettingsPage",
+	name: "SettingsPage",
 
-  setup() {
-    useMeta({
-      title: "Settings",
-      htmlAttrs: {
-        lang: "en",
-      },
-    });
-  },
+	setup() {
+		useMeta({
+			title: "Settings",
+			htmlAttrs: {
+				lang: "en",
+			},
+		});
+	},
 
-  data() {
-    return {
-      id: null,
-      username: "",
-      email: "",
-      fullName: "",
-      password: "",
-      confirmPassword: "",
-      countryId: null,
-      countries: null,
-      age: null,
-      message: "",
-      messageType: "",
-      oldUsername: "",
-      oldEmail: "",
-      oldFullName: "",
-      oldPassword: "",
-      oldConfirmPassword: "",
-      oldCountryId: null,
-      oldCountries: null,
-      oldAge: null,
-      isDeleted: false,
-    };
-  },
+	data() {
+		return {
+			usernameLabel: null,
+			emailLabel: null,
+			fullNameLabel: null,
+			ageLabel: null,
+			countries: null,
 
-  components: {
-    AlertComponent,
-  },
+			userId: null,
+			username: null,
+			email: null,
+			fullName: null,
+			password: null,
+			confirmPassword: null,
+			countryId: null,
+			age: null,
 
-  async created() {
-    this.$store.commit("setCurrentUrl", `/settings/${this.$route.params.id}`);
+			message: "",
+			messageType: ""
+		};
+	},
 
-    let countriesResponse = await this.getCountries();
-    this.countries = countriesResponse.body;
+	components: {
+		AlertComponent,
+	},
 
-    const token = await getAndValidateToken();
+	async created() {
+		this.$store.commit("setCurrentUrl", "/settings");
 
-    if (!token) {
-      this.$store.commit("setLogged", false);
-      this.$router.push("/login");
-    } else {
-      this.$store.commit("setLogged", true);
-      await this.getUserDetails(this.$route.params.id);
-    }
-  },
+		let countriesResponse = await this.getCountries();
+		this.countries = countriesResponse.body;
 
-  methods: {
-    async submitForm() {
-      const requestDto = {
-        updatedUsername:
-          this.username == this.oldUsername ? null : this.username,
-        updatedEmail: this.email == this.oldEmail ? null : this.email,
-        updatedFullName:
-          this.fullName == this.oldFullName ? null : this.fullName,
-        updatedPassword:
-          this.password == this.oldPassword ? null : this.password,
-        updatedConfirmPassword:
-          this.password == this.oldConfirmPassword
-            ? null
-            : this.confirmPassword,
-        updatedCountryId:
-          this.countryId == this.oldCountryId ? null : this.countryId,
-        updatedAge: this.age == this.oldAge ? null : this.age,
-      };
+		const token = await getAndValidateToken();
 
-      try {
-        const res = await this.updateUserDetails(requestDto);
+		if (!token) {
+			this.$store.commit("setLogged", false);
+			this.$router.push("/login");
+		} else {
+			this.$store.commit("setLogged", true);
+			await this.getUserDetails(this.$route.params.id);
+		}
+	},
 
-        if (res.status === 200) {
-          this.messageType = "SUCCESS";
-          this.message = "Profile has been updated successfully";
+	methods: {
+		async submitForm() {
+			const requestDto = {
+				username: this.username,
+				email: this.email,
+				fullName: this.fullName,
+				countryId: this.countryId,
+				age: this.age,
+				password: this.password,
+				confirmPassword: this.confirmPassword
+			};
 
-          this.username = res.body.username;
-          this.email = res.body.email;
-          this.fullName = res.body.fullName;
-          this.password = res.body.password;
-          this.confirmPassword = res.body.password;
-          this.countryId = res.body.countryId;
-          this.age = res.body.age;
-        } else {
-          let messageBuilder = "";
-          for (const key in res.body) {
-            messageBuilder += `${key}: ${res.body[key]}. `;
-          }
-          this.messageType = "WARNING";
-          this.message = `An error occured (${messageBuilder}Status ${res.status})`;
-        }
-      } catch (error) {
-        this.messageType = "WARNING";
-        this.message = `An error occurred (${error})`;
-      }
-    },
-    async getCountries() {
-      let URL = "/api/v1/users/countries";
+			try {
+				const res = await this.updateUserDetails(requestDto);
 
-      const res = await fetch(URL, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+				if (res.status === 200) {
+					this.messageType = "SUCCESS";
+					this.message = "Profile has been updated successfully";
 
-      const data = await res.json();
+					this.usernameLabel = res.body.username;
+					this.emailLabel = res.body.email;
+					this.fullNameLabel = res.body.fullName;
+					this.countryIdLabel = res.body.countryId;
+					this.ageLabel = res.body.age;
+				} else if (res.status === 401) {
+					localStorage.removeItem("token");
+					this.$store.commit("setLogged", false);
+					this.$router.push("/login");
+				} else {
+					let messageBuilder = "";
+					for (const key in res.body) {
+						messageBuilder += `${key}: ${res.body[key]}. `;
+					}
+					this.messageType = "WARNING";
+					this.message = `An error occured (${messageBuilder}Status ${res.status})`;
+				}
+			} catch (error) {
+				this.messageType = "WARNING";
+				this.message = `An error occurred (${error})`;
+			}
 
-      return {
-        status: res.status,
-        body: data,
-      };
-    },
+			this.username = null;
+			this.email = null;
+			this.fullName = null;
+			this.age = null;
+			this.password = null;
+			this.confirmPassword = null;
+		},
 
-    async submitFormDelete() {
-      try {
-        const res = await this.deleteUser(this.$route.params.id);
+		async submitFormDelete() {
+			try {
+				const res = await this.deleteUser(this.$route.params.id);
 
-        if (res.status === 204) {
-          this.messageType = "SUCCESS";
-          this.message = "Profile has been deleted successfully";
-          this.isDeleted = true;
-        } else {
-          this.messageType = "WARNING";
-          this.message = `An error occurred (${res.status})`;
-        }
-      } catch (error) {
-        this.messageType = "WARNING";
-        this.message = `An error occurred (${error})`;
-      }
-    },
+				if (res.status === 204) {
+					this.messageType = "SUCCESS";
+					this.message = "Profile has been deleted successfully";
+					localStorage.removeItem("token");
+					this.$store.commit("setLogged", false);
+					this.$router.push("/signup");
+				} else if (res.status === 401) {
+					localStorage.removeItem("token");
+					this.$store.commit("setLogged", false);
+					this.$router.push("/login");
+				} else {
+					let messageBuilder = "";
+					for (const key in res.body) {
+						messageBuilder += `${key}: ${res.body[key]}. `;
+					}
+					this.messageType = "WARNING";
+					this.message = `An error occured (${messageBuilder}Status ${res.status})`;
+				}
+			} catch (error) {
+				this.messageType = "WARNING";
+				this.message = `An error occurred (${error})`;
+			}
+		},
 
-    async getUserDetails() {
-      let URL = `/api/v1/users`;
-      let token = getToken();
+		async getCountries() {
+			let URL = "/api/v1/users/countries";
 
-      const res = await fetch(URL, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+			const res = await fetch(URL, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
 
-      const data = await res.json();
-      this.id = data.id;
-      this.username = data.username;
-      this.oldUsername = data.username;
-      this.email = data.email;
-      this.oldEmail = data.email;
-      this.fullName = data.fullName;
-      this.oldFullName = data.fullName;
-      this.password = data.password;
-      this.oldPassword = data.password;
-      this.confirmPassword = data.password;
-      this.oldConfirmPassword = data.password;
-      this.countryId = data.countryId;
-      this.oldCountryId = data.countryId;
-      this.age = data.age;
-      this.oldAge = data.age;
+			const data = await res.json();
 
-      this.$nextTick(() => {
-        this.$refs.username.placeholder = data.username;
-        this.$refs.email.placeholder = data.email;
-        this.$refs.fullName.placeholder = data.fullName;
-        this.$refs.password.placeholder = "********";
-        this.$refs.confirmPassword.placeholder = "********";
-        this.$refs.countryId.placeholder = data.countryId;
-        this.$refs.age.placeholder = data.age;
-      });
-    },
+			return {
+				status: res.status,
+				body: data,
+			};
+		},
 
-    async updateUserDetails(requestBody) {
-      let URL = `/api/v1/users/${this.id}`;
-      let token = getToken();
+		async getUserDetails() {
+			let URL = `/api/v1/users`;
+			let token = getToken();
 
-      const res = await fetch(URL, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(requestBody),
-      });
+			const res = await fetch(URL, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			});
 
-      const data = await res.json();
-      if (res.status === 200) {
-        this.id = data.id;
-        this.username = data.username;
-        this.oldUsername = data.username;
-        this.email = data.email;
-        this.oldEmail = data.email;
-        this.fullName = data.fullName;
-        this.oldFullName = data.fullName;
-        this.password = data.password;
-        this.oldPassword = data.password;
-        this.confirmPassword = data.password;
-        this.oldConfirmPassword = data.password;
-        this.countryId = data.countryId;
-        this.oldCountryId = data.countryId;
-        this.age = data.age;
-        this.oldAge = data.age;
-      }
+			const data = await res.json();
 
-      return {
-        status: res.status,
-        body: data,
-      };
-    },
+			this.userId = data.id;
+			this.usernameLabel = data.username;
+			this.emailLabel = data.email;
+			this.fullNameLabel = data.fullName;
+			this.ageLabel = data.age;
+			this.countryId = data.countryId;
+		},
 
-    async deleteUser() {
-      let URL = `/api/v1/users/${this.id}`;
-      let token = getToken();
+		async updateUserDetails(requestBody) {
+			let URL = `/api/v1/users/${this.userId}`;
+			let token = getToken();
 
-      const res = await fetch(URL, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      this.$store.commit("setCurrentUrl", "/login");
-      localStorage.removeItem("token");
-      this.$store.commit("setLogged", false);
-      this.$router.push("/login");
-      return {
-        status: res.status,
-      };
-    },
-  },
+			const res = await fetch(URL, {
+				method: "PATCH",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+				body: JSON.stringify(requestBody),
+			});
+
+			const data = await res.json();
+
+			return {
+				status: res.status,
+				body: data,
+			};
+		},
+
+		async deleteUser() {
+			let URL = `/api/v1/users/${this.userId}`;
+			let token = getToken();
+
+			const res = await fetch(URL, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			});
+
+			return {
+				status: res.status,
+			};
+		},
+	},
 };
 </script>
