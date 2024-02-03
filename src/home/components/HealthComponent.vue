07c6cd195de5c1059a8d45a60f1b797684177c56
Oleg Kucherenko <oleg.kucherenko.developer@gmail.com>
2024-02-03 11:06:07 +0200

    Update home page (195)

---
+++ src/home/components/HealthComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card mb-3 me-3 health-component-width">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p v-if="description" class="card-text">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HealthComponent",
+
+    props: {
+        title: String,
+        description: String
+    }
+}
+</script>
